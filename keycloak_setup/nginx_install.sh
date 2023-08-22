b4b96899b6ad4bb5b8ff47a7df083a11d40fa965
superbiscota <135531719+pj-tokyo@users.noreply.github.com>
2023-08-23 00:20:24 +0900

    nginx 設定変更

--- keycloak_setup/nginx_install.sh
+++ keycloak_setup/nginx_install.sh
@@ -6,7 +6,8 @@ sudo apt-get install nginx -y
 
 # Create a directory for the website
 sudo mkdir -p /var/www/ron-the-rocker.net
-echo "Hello, this is index.html" | sudo tee /var/www/ron-the-rocker.net/index.html
+echo "<html lang="ja"><head><meta charset="utf-8"></meta></head>....hello ? 🐟</html>" | sudo tee /var/www/ron-the-rocker.net/index.html
+echo "<html lang="ja"><head><meta charset="utf-8"></meta></head>🐟多分メンテ中</html>" | sudo tee /var/www/ron-the-rocker.net/maintenance.html
 
 # Create a server block configuration
 sudo tee /etc/nginx/sites-available/ron-the-rocker.net <<'EOF'
@@ -26,15 +27,21 @@ server {
     ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
 
+
     location / {
+        root /var/www/ron-the-rocker.net;
+        index index.html;
+        try_files $uri $uri/ =404;
+    }
+
+    location /auth {
         proxy_pass http://localhost:8180;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "Upgrade";
     }
+
 
     location /ndrr {
         proxy_pass http://localhost:7777;
@@ -45,9 +52,26 @@ server {
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection "Upgrade";
     }
+
+    location /maintenance.html {
+        root /var/www/ron-the-rocker.net;
+    }
+
+    error_page 502 /maintenance.html;
+    location = /maintenance.html {
+        root /var/www/ron-the-rocker.net;
+        internal;
+    }
+
 }
+
 EOF
+
 # Enable the server block
+# Remove the existing symlink if it exists
+if [ -L /etc/nginx/sites-enabled/ron-the-rocker.net ]; then
+    sudo rm /etc/nginx/sites-enabled/ron-the-rocker.net
+fi
 sudo ln -s /etc/nginx/sites-available/ron-the-rocker.net /etc/nginx/sites-enabled/
 
 # Test Nginx configuration
@@ -55,12 +79,6 @@ sudo nginx -t
 
 # Start Nginx service
 sudo systemctl start nginx
+sudo systemctl reload nginx
 
-# Install Certbot
-sudo apt-get install certbot python3-certbot-nginx -y
-
-# Obtain SSL certificate
-sudo certbot --nginx -d ron-the-rocker.net -d www.ron-the-rocker.net
-
-echo "Nginx installed, configured, and SSL certificate obtained. Access https://ron-the-rocker.net/ to see the secure website."
-
+echo "Nginx installed, configured, and running."
