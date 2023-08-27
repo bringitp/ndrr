417b42bfdc2190a2675c4ba2a396326fafe6c531
superbiscota <135531719+pj-tokyo@users.noreply.github.com>
2023-08-27 21:46:10 +0900

    react修正

--- chat_app/app/main.py
+++ chat_app/app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from chat_app.app.endpoints.root_endpoints import router as root_router
 from chat_app.app.endpoints.room_messages_post_endpoints import router as room_message_post_router
 from chat_app.app.endpoints.room_messages_get_endpoints import router as room_message_get_router
 from chat_app.app.endpoints.rooms_get_endpoints import router as rooms_get_router
 from fastapi.staticfiles import StaticFiles
 import os
+
 app = FastAPI()
+
+# CORS設定を行う
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ここに許可したいオリジンを指定
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # main.pyのディレクトリパスを取得
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -18,5 +29,3 @@ app.include_router(rooms_get_router)
 #app.include_router(rooms_post_router)
 app.include_router(room_message_get_router)
 app.include_router(room_message_post_router)
-
-
--- react_chat/src/App.js
+++ react_chat/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactKeycloakProvider, useKeycloak } from '@react-keycloak/web';
 import Keycloak from 'keycloak-js';
 
+const keycloakConfigLocal = {
+  url: 'https://ron-the-rocker.net/auth',
+  realm: 'ndrr',
+  clientId: 'react-local',
+  resource: 'test'
+};
 
-const keycloak = new Keycloak({
-  url: 'https://ron-the-rocker.net/auth', // Keycloakの認証URL
-  realm: 'ndrr',     // レルム名
-  clientId: 'react-local',   // クライアントID
-  resource: "test"
+const keycloakConfigServer = {
+  url: 'https://ron-the-rocker.net/auth',
+  realm: 'ndrr',
+  clientId: 'react-server',
+  resource: 'test'
+};
 
-});
+const useKeycloakConfig = process.env.REACT_APP_ENV === 'server' ? keycloakConfigServer : keycloakConfigLocal;
+
+const keycloak = new Keycloak(useKeycloakConfig);
 
 function App() {
   return (
@@ -21,6 +30,40 @@ function App() {
 
 function MainComponent() {
   const { keycloak, initialized } = useKeycloak();
+  const [jsonData, setJsonData] = useState(null);
+
+  useEffect(() => {
+    if (initialized && keycloak.authenticated) {
+      const apiUrl = window.location.href.startsWith('https://ron-the-rocker.net/ndrr/static')
+        ? 'https://ron-the-rocker.net/ndrr/api/rooms/1/messages'
+        : 'http://localhost:7777/rooms/1/messages';
+
+      const headers = new Headers();
+      headers.append('Authorization', `Bearer ${keycloak.token}`);
+
+      const fetchData = async () => {
+        try {
+          const response = await fetch(apiUrl, { headers });
+          if (response.ok) {
+            const data = await response.json();
+            setJsonData(data);
+          } else {
+            console.error('Error fetching data:', response.status);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+      // Fetch data initially and set interval for regular updates
+      fetchData();
+      const fetchInterval = setInterval(fetchData, 2000);
+
+      return () => {
+        clearInterval(fetchInterval);
+      };
+    }
+  }, [initialized, keycloak.authenticated]);
 
   if (!initialized) {
     return <div>Loading...</div>;
@@ -32,8 +75,14 @@ function MainComponent() {
 
   return (
     <div>
-      <p>Welcome, {keycloak.tokenParsed.preferred_username}!</p>
+      <p>Welcome</p>
       <button onClick={() => keycloak.logout()}>Logout</button>
+
+      {jsonData ? (
+        <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+      ) : (
+        <div>Loading JSON data...</div>
+      )}
     </div>
   );
 }
