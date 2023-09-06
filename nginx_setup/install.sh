984aafe147e9cd00fd298022e94d1894f7e616ce
superbiscota <135531719+pj-tokyo@users.noreply.github.com>
2023-09-06 18:16:08 +0900

    3coreだとエラーが発生するので２コアへ

--- nginx_setup/install.sh
+++ nginx_setup/install.sh
@@ -23,7 +23,7 @@ server {
     upstream backend {
         server 127.0.0.1:7777;  # バックエンドサーバーのポート
         server 127.0.0.1:7778;  # 追加のバックエンドサーバーのポート
-        server 127.0.0.1:7779;  # 追加のバックエンドサーバーのポート
+
         # さらに必要な数だけサーバーを追加
     }
 
@@ -109,46 +109,6 @@ server {
             proxy_set_header Connection "Upgrade";
         }
     }
-
-
-    server {
-        listen 443 ssl;
-        server_name ron-the-rocker.net www.ron-the-rocker.net;
-
-        ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
-
-        location / {
-            root /var/www/ron-the-rocker.net;
-            index index.html;
-            try_files $uri $uri/ /index.html;
-        }
-
-        location ~ ^/ndrr/api {
-            rewrite ^/ndrr/api(.*)?$ $1 break;
-
-            add_header Access-Control-Allow-Origin http://localhost:3000;
-            add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
-            add_header Access-Control-Allow-Headers 'Origin, Content-Type, Accept, Authorization';
-
-            if ($request_method = 'OPTIONS') {
-                add_header Access-Control-Allow-Credentials 'true';
-                add_header Access-Control-Max-Age 1728000;
-                add_header Content-Type 'text/plain charset=UTF-8';
-                add_header Content-Length 0;
-                return 204;
-            }
-
-            proxy_pass http://backend;  # upstreamで定義したバックエンドへのプロキシ
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "Upgrade";
-        }
-    }
-
 
   # リクエストされたリソ
 EOF
