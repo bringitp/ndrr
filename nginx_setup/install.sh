770042c8a434524280168136288c2edd387daa5d
superbiscota <135531719+pj-tokyo@users.noreply.github.com>
2023-09-06 17:17:15 +0900

    Update install.sh

--- nginx_setup/install.sh
+++ nginx_setup/install.sh
@@ -23,23 +23,31 @@ server {
     upstream backend {
         server 127.0.0.1:7777;  # バックエンドサーバーのポート
         server 127.0.0.1:7778;  # 追加のバックエンドサーバーのポート
+        server 127.0.0.1:7779;  # 追加のバックエンドサーバーのポート
         # さらに必要な数だけサーバーを追加
     }
 
-server {
+  server {
     listen 443 ssl;
     server_name ron-the-rocker.net www.ron-the-rocker.net;
 
     ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
-
+   
     location / {
         root /var/www/ron-the-rocker.net;
         index index.html;
         try_files $uri $uri/ /index.html;
     }
 
-       location ~ ^/ndrr/api { 
+    location /auth {
+        proxy_pass http://localhost:8180;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+    location ~ ^/ndrr/api { 
        rewrite ^/ndrr/api(.*)?$ $1 break;
 
         add_header Access-Control-Allow-Origin http://localhost:3000;
@@ -54,7 +62,7 @@ server {
             return 204;
         }
 
-        proxy_pass http://localhost:7777;
+        proxy_pass http://backend;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -62,7 +70,46 @@ server {
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection "Upgrade";
     }
-}
+  }
+
+    server {
+        listen 443 ssl;
+        server_name ron-the-rocker.net www.ron-the-rocker.net;
+
+        ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
+
+        location / {
+            root /var/www/ron-the-rocker.net;
+            index index.html;
+            try_files $uri $uri/ /index.html;
+        }
+
+        location ~ ^/ndrr/api {
+            rewrite ^/ndrr/api(.*)?$ $1 break;
+
+            add_header Access-Control-Allow-Origin http://localhost:3000;
+            add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
+            add_header Access-Control-Allow-Headers 'Origin, Content-Type, Accept, Authorization';
+
+            if ($request_method = 'OPTIONS') {
+                add_header Access-Control-Allow-Credentials 'true';
+                add_header Access-Control-Max-Age 1728000;
+                add_header Content-Type 'text/plain charset=UTF-8';
+                add_header Content-Length 0;
+                return 204;
+            }
+
+            proxy_pass http://backend;  # upstreamで定義したバックエンドへのプロキシ
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "Upgrade";
+        }
+    }
+
 
     server {
         listen 443 ssl;
@@ -121,5 +168,4 @@ sudo systemctl start nginx
 sudo systemctl reload nginx
 
 echo "Nginx installed, configured, and running."
-
 
