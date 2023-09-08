9a8ed9566724ddfb07e1234c2c0912ffa69a1633
superbiscota <135531719+pj-tokyo@users.noreply.github.com>
2023-09-08 12:27:28 +0900

    nginx refactor

--- nginx_setup/install.sh
+++ nginx_setup/install.sh
@@ -30,28 +30,38 @@ server {
         # さらに必要な数だけサーバーを追加
     }
 
-  server {
+server {
     listen 443 ssl;
     server_name ron-the-rocker.net www.ron-the-rocker.net;
 
     ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
-   
+
+    brotli on;
+    brotli_static on;
+    brotli_comp_level 6;
+    brotli_types text/plain text/css application/json application/javascript application/xml application/xhtml+xml image/svg+xml application/rss+xml application/atom+xml image/x-icon image/vnd.microsoft.icon image/jpeg image/png image/gif;
+
+    # すべてのリソースにBrotli圧縮を適用
     location / {
         root /var/www/ron-the-rocker.net;
         index index.html;
         try_files $uri $uri/ /index.html;
+        brotli_types text/html application/javascript;
     }
 
+    # /auth および /ndrr/api にもBrotli圧縮を適用
     location /auth {
         proxy_pass http://localhost:8180;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
+        brotli_types text/html application/javascript;
     }
+
     location ~ ^/ndrr/api { 
-       rewrite ^/ndrr/api(.*)?$ $1 break;
+        rewrite ^/ndrr/api(.*)?$ $1 break;
 
         add_header Access-Control-Allow-Origin http://localhost:3000;
         add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
@@ -72,162 +82,233 @@ server {
         proxy_set_header X-Forwarded-Proto $scheme;
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection "Upgrade";
+        brotli_types text/html application/javascript;
     }
-  }
+}
 
-    server {
-        listen 443 ssl;
-        server_name ron-the-rocker.net www.ron-the-rocker.net;
+server {
+    listen 443 ssl;
+    server_name ron-the-rocker.net www.ron-the-rocker.net;
 
-        ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
+    ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
 
-        location / {
-            root /var/www/ron-the-rocker.net;
-            index index.html;
-            try_files $uri $uri/ /index.html;
-        }
+    brotli on;
+    brotli_static on;
+    brotli_comp_level 6;
+    brotli_types text/plain text/css application/json application/javascript application/xml application/xhtml+xml image/svg+xml application/rss+xml application/atom+xml image/x-icon image/vnd.microsoft.icon image/jpeg image/png image/gif;
 
-        location ~ ^/ndrr/api {
-            rewrite ^/ndrr/api(.*)?$ $1 break;
-
-            add_header Access-Control-Allow-Origin http://localhost:3000;
-            add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
-            add_header Access-Control-Allow-Headers 'Origin, Content-Type, Accept, Authorization';
-
-            if ($request_method = 'OPTIONS') {
-                add_header Access-Control-Allow-Credentials 'true';
-                add_header Access-Control-Max-Age 1728000;
-                add_header Content-Type 'text/plain charset=UTF-8';
-                add_header Content-Length 0;
-                return 204;
-            }
-
-            proxy_pass http://backend;  # upstreamで定義したバックエンドへのプロキシ
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "Upgrade";
-        }
+    # すべてのリソースにBrotli圧縮を適用
+    location / {
+        root /var/www/ron-the-rocker.net;
+        index index.html;
+        try_files $uri $uri/ /index.html;
+        brotli_types text/html application/javascript;
     }
 
-    server {
-        listen 443 ssl;
-        server_name ron-the-rocker.net www.ron-the-rocker.net;
-
-        ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
-
-        location / {
-            root /var/www/ron-the-rocker.net;
-            index index.html;
-            try_files $uri $uri/ /index.html;
-        }
-
-        location ~ ^/ndrr/api {
-            rewrite ^/ndrr/api(.*)?$ $1 break;
-
-            add_header Access-Control-Allow-Origin http://localhost:3000;
-            add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
-            add_header Access-Control-Allow-Headers 'Origin, Content-Type, Accept, Authorization';
-
-            if ($request_method = 'OPTIONS') {
-                add_header Access-Control-Allow-Credentials 'true';
-                add_header Access-Control-Max-Age 1728000;
-                add_header Content-Type 'text/plain charset=UTF-8';
-                add_header Content-Length 0;
-                return 204;
-            }
-
-            proxy_pass http://backend;  # upstreamで定義したバックエンドへのプロキシ
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "Upgrade";
-        }
+    # /auth および /ndrr/api にもBrotli圧縮を適用
+    location /auth {
+        proxy_pass http://localhost:8180;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        brotli_types text/html application/javascript;
     }
+
+    location ~ ^/ndrr/api { 
+        rewrite ^/ndrr/api(.*)?$ $1 break;
+
+        add_header Access-Control-Allow-Origin http://localhost:3000;
+        add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
+        add_header Access-Control-Allow-Headers 'Origin, Content-Type, Accept, Authorization';
+
+        if ($request_method = 'OPTIONS') {
+            add_header Access-Control-Allow-Credentials 'true';
+            add_header Access-Control-Max-Age 1728000;
+            add_header Content-Type 'text/plain charset=UTF-8';
+            add_header Content-Length 0;
+            return 204;
+        }
+
+        proxy_pass http://backend;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        brotli_types text/html application/javascript;
+    }
+}
+server {
+    listen 443 ssl;
+    server_name ron-the-rocker.net www.ron-the-rocker.net;
+
+    ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
+
+    brotli on;
+    brotli_static on;
+    brotli_comp_level 6;
+    brotli_types text/plain text/css application/json application/javascript application/xml application/xhtml+xml image/svg+xml application/rss+xml application/atom+xml image/x-icon image/vnd.microsoft.icon image/jpeg image/png image/gif;
+
+    # すべてのリソースにBrotli圧縮を適用
+    location / {
+        root /var/www/ron-the-rocker.net;
+        index index.html;
+        try_files $uri $uri/ /index.html;
+        brotli_types text/html application/javascript;
+    }
+
+    # /auth および /ndrr/api にもBrotli圧縮を適用
+    location /auth {
+        proxy_pass http://localhost:8180;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        brotli_types text/html application/javascript;
+    }
+
+    location ~ ^/ndrr/api { 
+        rewrite ^/ndrr/api(.*)?$ $1 break;
+
+        add_header Access-Control-Allow-Origin http://localhost:3000;
+        add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
+        add_header Access-Control-Allow-Headers 'Origin, Content-Type, Accept, Authorization';
+
+        if ($request_method = 'OPTIONS') {
+            add_header Access-Control-Allow-Credentials 'true';
+            add_header Access-Control-Max-Age 1728000;
+            add_header Content-Type 'text/plain charset=UTF-8';
+            add_header Content-Length 0;
+            return 204;
+        }
+
+        proxy_pass http://backend;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        brotli_types text/html application/javascript;
+    }
+}
     
-        server {
-        listen 443 ssl;
-        server_name ron-the-rocker.net www.ron-the-rocker.net;
+server {
+    listen 443 ssl;
+    server_name ron-the-rocker.net www.ron-the-rocker.net;
 
-        ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
+    ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
 
-        location / {
-            root /var/www/ron-the-rocker.net;
-            index index.html;
-            try_files $uri $uri/ /index.html;
-        }
+    brotli on;
+    brotli_static on;
+    brotli_comp_level 6;
+    brotli_types text/plain text/css application/json application/javascript application/xml application/xhtml+xml image/svg+xml application/rss+xml application/atom+xml image/x-icon image/vnd.microsoft.icon image/jpeg image/png image/gif;
 
-        location ~ ^/ndrr/api {
-            rewrite ^/ndrr/api(.*)?$ $1 break;
-
-            add_header Access-Control-Allow-Origin http://localhost:3000;
-            add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
-            add_header Access-Control-Allow-Headers 'Origin, Content-Type, Accept, Authorization';
-
-            if ($request_method = 'OPTIONS') {
-                add_header Access-Control-Allow-Credentials 'true';
-                add_header Access-Control-Max-Age 1728000;
-                add_header Content-Type 'text/plain charset=UTF-8';
-                add_header Content-Length 0;
-                return 204;
-            }
-
-            proxy_pass http://backend;  # upstreamで定義したバックエンドへのプロキシ
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "Upgrade";
-        }
+    # すべてのリソースにBrotli圧縮を適用
+    location / {
+        root /var/www/ron-the-rocker.net;
+        index index.html;
+        try_files $uri $uri/ /index.html;
+        brotli_types text/html application/javascript;
     }
+
+    # /auth および /ndrr/api にもBrotli圧縮を適用
+    location /auth {
+        proxy_pass http://localhost:8180;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        brotli_types text/html application/javascript;
+    }
+
+    location ~ ^/ndrr/api { 
+        rewrite ^/ndrr/api(.*)?$ $1 break;
+
+        add_header Access-Control-Allow-Origin http://localhost:3000;
+        add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
+        add_header Access-Control-Allow-Headers 'Origin, Content-Type, Accept, Authorization';
+
+        if ($request_method = 'OPTIONS') {
+            add_header Access-Control-Allow-Credentials 'true';
+            add_header Access-Control-Max-Age 1728000;
+            add_header Content-Type 'text/plain charset=UTF-8';
+            add_header Content-Length 0;
+            return 204;
+        }
+
+        proxy_pass http://backend;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        brotli_types text/html application/javascript;
+    }
+}
     
-        server {
-        listen 443 ssl;
-        server_name ron-the-rocker.net www.ron-the-rocker.net;
+server {
+    listen 443 ssl;
+    server_name ron-the-rocker.net www.ron-the-rocker.net;
 
-        ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
+    ssl_certificate /etc/letsencrypt/live/ron-the-rocker.net/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/ron-the-rocker.net/privkey.pem;
 
-        location / {
-            root /var/www/ron-the-rocker.net;
-            index index.html;
-            try_files $uri $uri/ /index.html;
-        }
+    brotli on;
+    brotli_static on;
+    brotli_comp_level 6;
+    brotli_types text/plain text/css application/json application/javascript application/xml application/xhtml+xml image/svg+xml application/rss+xml application/atom+xml image/x-icon image/vnd.microsoft.icon image/jpeg image/png image/gif;
 
-        location ~ ^/ndrr/api {
-            rewrite ^/ndrr/api(.*)?$ $1 break;
-
-            add_header Access-Control-Allow-Origin http://localhost:3000;
-            add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
-            add_header Access-Control-Allow-Headers 'Origin, Content-Type, Accept, Authorization';
-
-            if ($request_method = 'OPTIONS') {
-                add_header Access-Control-Allow-Credentials 'true';
-                add_header Access-Control-Max-Age 1728000;
-                add_header Content-Type 'text/plain charset=UTF-8';
-                add_header Content-Length 0;
-                return 204;
-            }
-
-            proxy_pass http://backend;  # upstreamで定義したバックエンドへのプロキシ
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "Upgrade";
-        }
+    # すべてのリソースにBrotli圧縮を適用
+    location / {
+        root /var/www/ron-the-rocker.net;
+        index index.html;
+        try_files $uri $uri/ /index.html;
+        brotli_types text/html application/javascript;
     }
 
-  # リクエストされたリソ
+    # /auth および /ndrr/api にもBrotli圧縮を適用
+    location /auth {
+        proxy_pass http://localhost:8180;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        brotli_types text/html application/javascript;
+    }
+
+    location ~ ^/ndrr/api { 
+        rewrite ^/ndrr/api(.*)?$ $1 break;
+
+        add_header Access-Control-Allow-Origin http://localhost:3000;
+        add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
+        add_header Access-Control-Allow-Headers 'Origin, Content-Type, Accept, Authorization';
+
+        if ($request_method = 'OPTIONS') {
+            add_header Access-Control-Allow-Credentials 'true';
+            add_header Access-Control-Max-Age 1728000;
+            add_header Content-Type 'text/plain charset=UTF-8';
+            add_header Content-Length 0;
+            return 204;
+        }
+
+        proxy_pass http://backend;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        brotli_types text/html application/javascript;
+    }
+}
+
 EOF
 
 # Enable the server block
