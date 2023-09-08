e40be19f32b448d9d5da1f8cb3e8241d1b8c4edb
superbiscota <135531719+pj-tokyo@users.noreply.github.com>
2023-09-08 20:35:33 +0900

    5  cores

--- chat_app/app/auth/curl_messages_get_ab.py
+++ chat_app/app/auth/curl_messages_get_ab.py
@@ -1,19 +1,19 @@
 import subprocess
 from config import TOKEN  # config.py ファイルからトークンをインポート
 
-#url = "http://localhost:7777/room/1/messages"
+# url = "http://localhost:7777/room/1/messages"
 url = "https://ron-the-rocker.net/ndrr/api/room/1/messages"
-headers = {
-    "Authorization": f"Bearer {TOKEN}"
-}
+headers = {"Authorization": f"Bearer {TOKEN}"}
 
 # abコマンドを実行するためのコマンドラインを構築
-ab_command = f"ab -n 1000 -c 4 -H 'Authorization: {headers['Authorization']}' {url}"
+ab_command = f"ab -n 140 -c 10 -l -H 'Authorization: {headers['Authorization']}'   {url}"
 
 try:
-    # abコマンドを実行し、結果を取得
-    result = subprocess.check_output(ab_command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
-    
+    # abコマンドを実行し、結果を取得|||
+    result = subprocess.check_output(
+        ab_command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True
+    )
+
     # 結果を出力
     print(result)
 except subprocess.CalledProcessError as e:
