92bc4f0f8c47516d4d76f13426a9c60a6c79c1cc
superbiscota <135531719+pj-tokyo@users.noreply.github.com>
2023-09-09 17:50:33 +0900

    cached

--- chat_app/app/auth_utils.py
+++ chat_app/app/auth_utils.py
@@ -3,8 +3,10 @@ import jwt
 from fastapi import HTTPException, status,Depends
 from chat_app.app.database.models import User,UserNGList
 from sqlalchemy.orm import Session
-
-   
+import cachetools
+from datetime import datetime
+# キャッシュの設定（10秒のTTLキャッシュ）
+token_cache = cachetools.TTLCache(maxsize=1000, ttl=20)
 
 class UserToken:
     sub: str
@@ -16,11 +18,27 @@ class LoginUser(UserToken):
     avatar: str
 
 def validate_token(token_string: str, public_key: str) -> str:
+    # キャッシュからトークンの検証結果を取得
+    cached_sub = token_cache.get(token_string)
+    if cached_sub:
+        return cached_sub
+
     options = {"verify_signature": True, "verify_aud": False, "exp": True}
     payload = jwt.decode(token_string, public_key, algorithms=["RS256"], options=options)
     sub = payload.get("sub")
     if not sub:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid token payload")
+
+    # トークンを検証し、有効期限を確認
+    exp_timestamp = payload.get("exp")
+    if exp_timestamp:
+        current_timestamp = datetime.timestamp(datetime.now())
+        if current_timestamp >= exp_timestamp:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
+
+    # キャッシュにトークン検証結果を保存
+    token_cache[token_string] = sub
+
     return sub
 
 def get_user_by_sub(sub: str, db: Session) -> User:
